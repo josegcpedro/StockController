ed2b1f36ff854b3a3643bc4bb2220ad48c891cbb
Jose <pedrojcunha7@hotmail.com>
2025-01-16 10:18:45 +0100

    feat: login functionnality with verification process with attempts

--- StockController/Program.cs
+++ StockController/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using dotenv.net;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        VerifyIdentification();
+    }
+
+    static void VerifyIdentification()
+    {
+        Console.WriteLine("Veuillez vous identifier");
+
+        DotEnv.Load();
+
+        for (int attempts = 0; attempts < 3; attempts++)
+        {
+            string? password = Console.ReadLine();
+            string? verificationPassword = Environment.GetEnvironmentVariable("password");
+            if (password != verificationPassword)
+            {
+                Console.WriteLine("Mot de passe incorrect");
+                if (attempts == 2)
+                {
+                    Console.WriteLine("Tentatives épuisées !");
+                    return;
+                }
+            }
+            else
+            {
+                Menu();
+                return;
+            }
+        }
+    }
+
+    static void Menu()
+    {
+        Console.WriteLine("hallo");
+    }
+}
