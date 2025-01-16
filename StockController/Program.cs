b420a75370f21336b216542831ed1887a57b20b4
Jose <pedrojcunha7@hotmail.com>
2025-01-16 11:18:13 +0100

    feat: add product logic and search by category logic

--- StockController/Program.cs
+++ StockController/Program.cs
@@ -3,6 +3,8 @@ using System;
 
 class Program
 {
+    static List<Product> products = new List<Product>();
+
     static void Main()
     {
         VerifyIdentification();
@@ -37,12 +39,12 @@ class Program
 
     static void Menu()
     {
-        Console.WriteLine("Bonjour! Que souhaitez vous faire!");
+        Console.WriteLine("Que souhaitez vous faire!");
 
         Console.WriteLine("1. Ajouter un produit");
         Console.WriteLine("2. Modifier un produit");
         Console.WriteLine("3. Suprimmer un produit");
-        Console.WriteLine("4. Afficher un ou les produits");
+        Console.WriteLine("4. Afficher les produits d'une categorie");
         Console.WriteLine("5. Quitter");
 
         string? choix = Console.ReadLine();
@@ -51,7 +53,7 @@ class Program
         {
             case "1":
                 Console.WriteLine("1");
-                //AddProduct()
+                AddProduct();
                 break;
             case "2":
                 Console.WriteLine("2");
@@ -62,20 +64,84 @@ class Program
                 //AddProduct()
                 break;
             case "4":
-                Console.WriteLine("1. Afficher un produit");
-                Console.WriteLine("2. Afficher tous les produit");
-                string? choixProduits = Console.ReadLine();
-                if (choixProduits == "1")
-                {
-                    //ShowOneProduct();
-                    Console.WriteLine("1");
-                } else if (choixProduits == "2"){
-                    Console.WriteLine("2");
-                }
-
+                ShowProductByCategory();
                 break;
             case "5":
                 return;
         }
+    }
+
+
+    static void AddProduct()
+    {
+        Console.WriteLine("Quel est le nom du produit?");
+        string? productName = Console.ReadLine();
+        Console.WriteLine("Prix du produit :");
+        decimal productPrice;
+
+        if (!decimal.TryParse(Console.ReadLine(), out productPrice))
+        {
+            Console.WriteLine("Prix invalide.");
+            return;
+        }
+        Console.WriteLine("Quel est la categorie du produit?");
+        string? categorie = Console.ReadLine();
+
+        Product product = new Product(categorie, productName, productPrice);
+        products.Add(product);
+        Menu();
+    }
+
+    static void ShowProductByCategory()
+    {
+        Console.WriteLine("Quel est le nom de la categorie que vous souhaitez afficher les produits?");
+        string choix = Console.ReadLine();
+
+        var filteredProducts = products.Where(p => p.Categorie.Equals(choix, StringComparison.OrdinalIgnoreCase)).ToList();
+
+        if (filteredProducts.Any())
+        {
+            foreach (var product in products)
+            {
+                Console.WriteLine(product);
+            }
+            Menu();
+        }
+        else
+        {
+            Console.WriteLine("Pas de produit de cette categorie trouvé");
+            Menu();
+        }
+    }
+}
+
+class Product
+{
+    public string? ProductName { get; set; }
+    public decimal? ProductPrice { get; set; }
+    public string? Categorie { get; set; }
+
+
+
+    public Product(string categorie, string productName, decimal productPrice)
+    {
+        Categorie = categorie;
+        ProductName = productName;
+        ProductPrice = productPrice;
+    }
+
+    public override string ToString()
+    {
+        return $"Nom: {ProductName}, Prix: {ProductPrice:C}, Catégorie: {Categorie}";
+    }
+}
+
+class Categorie
+{
+    public string NomCategorie { get; set; }
+
+    public Categorie(string nomCategorie)
+    {
+        NomCategorie = nomCategorie;
     }
 }
