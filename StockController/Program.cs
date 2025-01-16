95438c6c81512dc577af4daa8753d4f005f7ffad
Jose <pedrojcunha7@hotmail.com>
2025-01-16 10:29:01 +0100

    feat: set up menu

--- StockController/Program.cs
+++ StockController/Program.cs
@@ -37,6 +37,45 @@ class Program
 
     static void Menu()
     {
-        Console.WriteLine("hallo");
+        Console.WriteLine("Bonjour! Que souhaitez vous faire!");
+
+        Console.WriteLine("1. Ajouter un produit");
+        Console.WriteLine("2. Modifier un produit");
+        Console.WriteLine("3. Suprimmer un produit");
+        Console.WriteLine("4. Afficher un ou les produits");
+        Console.WriteLine("5. Quitter");
+
+        string? choix = Console.ReadLine();
+
+        switch (choix)
+        {
+            case "1":
+                Console.WriteLine("1");
+                //AddProduct()
+                break;
+            case "2":
+                Console.WriteLine("2");
+                //AddProduct()
+                break;
+            case "3":
+                Console.WriteLine("3");
+                //AddProduct()
+                break;
+            case "4":
+                Console.WriteLine("1. Afficher un produit");
+                Console.WriteLine("2. Afficher tous les produit");
+                string? choixProduits = Console.ReadLine();
+                if (choixProduits == "1")
+                {
+                    //ShowOneProduct();
+                    Console.WriteLine("1");
+                } else if (choixProduits == "2"){
+                    Console.WriteLine("2");
+                }
+
+                break;
+            case "5":
+                return;
+        }
     }
 }
